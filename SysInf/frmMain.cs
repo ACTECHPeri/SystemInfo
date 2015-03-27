3be93d29c96292778a9f9f205756bfbce079a664
Periasamy K <periasamy@actechsoft.com>
2015-03-27 10:35:40 +0530

    Removing unwanted lines.

--- SysInf/frmMain.cs
+++ SysInf/frmMain.cs
@@ -20,7 +20,6 @@ namespace SysInf
         {
             string output = ServerSystem.GetMachinePerformance();
             lblSysInf.Text = output;
-            //MessageBox.Show(output);
         }
     }
 }
